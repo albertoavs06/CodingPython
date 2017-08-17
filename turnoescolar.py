70069abceadb0018d5dcb89a01ff3d7afda5e002
Rayana <rayana.azus@gmail.com>
2017-08-17 16:46:05 -0300

    add

---
+++ turnoescolar.py
@@ -0,0 +1,9 @@
+turno = raw_input("Que turno você estuda? ")
+if turno == "M" or turno == "m" :
+    print "Matutino"
+elif turno == "V" or turno == "v" :
+    print "Vespertino"
+elif turno == "N" or turno == "n" :
+    print "Noturno"
+else :
+    print "Turno inválido"
